a63b2b0ed69c763c4b45fc4b5dfa3bb07a69711f
neuro-giupo <giuseppe.pon@gmail.com>
2026-02-05 09:35:46 +0100

    Initial bulk commit

--- src/preprocess.py
+++ src/preprocess.py
@@ -14,15 +14,15 @@ def preprocess_t1w(img_path):
     image = ants.image_read(img_path)
     image_preprocessing = antspynet.preprocess_brain_image(image, 
                                                 truncate_intensity=(0.01, 0.99), 
-                                                brain_extraction_modality=None,
+                                                brain_extraction_modality="t1",
                                                 template="croppedMni152",
                                                 template_transform_type="antsRegistrationSyNQuickRepro[a]",
                                                 do_bias_correction=True,
                                                 do_denoising=True,
                                                 verbose=True)
     preprocessed_image = image_preprocessing["preprocessed_image"]
-    # preprocessed_image_brain = image_preprocessing["preprocessed_image"] * image_preprocessing['brain_mask']
-    return preprocessed_image
+    preprocessed_image_brain = image_preprocessing["preprocessed_image"] * image_preprocessing['brain_mask']
+    return preprocessed_image_brain
 
 def preprocess_flair(img_path):
     # use FLAIR-specific template (https://brainder.org/download/flair/)
